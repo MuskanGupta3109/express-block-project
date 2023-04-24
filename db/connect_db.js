6e486ec1b1fa4b6522aa53bf948327730796e504
MuskanGupta3109 <muskangupta9224@gmail.com>
2023-04-24 19:53:31 +0530

    done

--- db/connect_db.js
+++ db/connect_db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 
-const uri="mongodb+srv://muskangupta3109:muskan3109@cluster0.x00tttt.mongodb.net/Blogwebsite?retryWrites=true&w=majority"
+const uri="mongodb+srv://muskangupta3109:Muskan123@cluster0.x00tttt.mongodb.net/Blogwebsite?retryWrites=true&w=majority"
 const connectDB=()=>{
     //return mongoose.connect('mongodb://localhost:27017/Blogwebsite')
     return mongoose.connect(uri)
